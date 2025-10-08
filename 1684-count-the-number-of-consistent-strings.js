1bc53b1007db6f90f4d6dcf065939117a9e80030
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2025-10-08 21:32:46 +0530

    Create 1684-count-the-number-of-consistent-strings.js

---
+++ 1684-count-the-number-of-consistent-strings.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+var countConsistentStrings = function(allowed, words) {
+let count = 0;
+// let allowed = "ab";
+let allowedStringLength = allowed.length;
+console.log('allowedStringLength', allowedStringLength);
+
+// let words = ["ad", "aaab"];
+// let words = ["ad","bd","aaab","baa","badab"]
+
+words.forEach((word) => {
+  let found = 0; 
+  console.log('word', word);
+
+  for (let i = 0; i < word.length; i++) {
+    if (allowed.includes(word[i])) {
+      found = found + 1;
+    } 
+    else {
+      found = -1; 
+      break;
+    }
+  }
+
+  if (found > 0 && found !== -1) {
+    console.log('word:', word, 'is consistent');
+    count = count + 1;
+  } else {
+    console.log('word:', word, 'is not consistent');
+  }
+});
+
+console.log('consistent strings in the array words', count);
+return count;
+};
