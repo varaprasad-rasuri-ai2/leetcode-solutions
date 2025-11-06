481dc29a721c9a1b22a6b8488b41835b8a277f21
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2025-11-06 22:45:00 +0530

    Create 1-two-sum.js

---
+++ 1-two-sum.js
@@ -0,0 +1,33 @@
+var twoSum = function (nums, target) {
+    let ans = [];
+
+    // Loop through each number in the array
+    nums.forEach((num, i) => {
+        // Check every number after the current one
+        for (let j = i + 1; j < nums.length; j++) {
+            // If the sum equals the target, store indices
+            if ((num + nums[j]) === target) {
+                ans = [i, j];
+                return; // Exits the inner function, not the outer forEach
+            }
+        }
+    });
+
+    return ans;
+};
+
+
+var twoSum222 = function (nums, target) {
+    let ans = [];
+
+    nums.forEach((num, i) => {
+        if ((nums[i] + nums[i + 1]) === target) {
+            ans.push(i);
+            ans.push(i + 1);
+        }
+    });
+
+    return ans;
+};
+
+
