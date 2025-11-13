e20b76ed7aa2140d075d1445d11ae51a423a2c76
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2025-11-13 23:31:45 +0530

    Create 344-reverse-string.js

---
+++ 344-reverse-string.js
@@ -0,0 +1,6 @@
+var reverseString = function (s) {
+    // The input 's' is an array of characters.
+    // The built-in Array method 'reverse()' reverses the array *in place*,
+    // meaning it modifies the original array rather than returning a new one.
+    s.reverse();
+};
