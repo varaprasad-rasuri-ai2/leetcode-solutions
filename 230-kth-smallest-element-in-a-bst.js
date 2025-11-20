d86ac6f01ebae743da3229daf45c58c397441296
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2025-11-20 19:03:05 +0530

    Create 230-kth-smallest-element-in-a-bst.js

---
+++ 230-kth-smallest-element-in-a-bst.js
@@ -0,0 +1,30 @@
+var kthSmallest = function (root, k) {
+    // Will hold the result once the k-th smallest value is found
+    let kthSmallestVal = -1;
+
+    // In-order DFS traversal (Left → Node → Right)
+    // In a BST, this visits nodes in ascending sorted order
+    function dfs(n) {
+        if (!n) return;
+
+        // Traverse left subtree first
+        dfs(n.left);
+
+        // Visit current node:
+        // decrement k each time we visit a node in sorted order
+        --k;
+
+        // When k reaches 0, we've found the k-th smallest element
+        if (k === 0) {
+            kthSmallestVal = n.val;
+            return; // early exit from this branch
+        }
+
+        // Traverse right subtree
+        dfs(n.right);
+    }
+
+    dfs(root);
+
+    return kthSmallestVal;
+};
