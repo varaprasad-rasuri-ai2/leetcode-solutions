a5d379e34ac2221f5ea042c4772193940ee0bc4c
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2025-12-29 22:57:10 +0530

    Create 3689-maximum-total-subarray-value-i.js

---
+++ 3689-maximum-total-subarray-value-i.js
@@ -0,0 +1,9 @@
+var maxTotalValue = function (nums, k) {
+    let maxNum = Math.max(...nums)   // Find the largest number in the array
+    let minNum = Math.min(...nums)   // Find the smallest number in the array
+    let maxValue = maxNum - minNum   // Maximum possible subarray value
+
+    return k * maxValue              // Multiply by k because we can pick the same subarray k times
+};
+
+
