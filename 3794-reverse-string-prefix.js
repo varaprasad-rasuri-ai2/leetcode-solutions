33e1bca02afaf2f572e3f8aaa13084bb4b272aa6
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2026-01-16 06:20:35 +0530

    Create 3794-reverse-string-prefix.js

---
+++ 3794-reverse-string-prefix.js
@@ -0,0 +1,21 @@
+var reversePrefix = function (s, k) {
+    // Extract the first k characters of the string
+    let firstPartArr = s.slice(0, k);
+
+    // Extract the remaining part of the string starting from index k
+    let remainingPartStr = s.slice(k);
+
+    // Reverse the first part:
+    // 1. Convert string to array of characters
+    // 2. Reverse the array
+    // 3. Join back into a string
+    let firstPartStr = firstPartArr.split("").reverse().join("");
+
+    // Concatenate the reversed prefix with the remaining string
+    return firstPartStr + remainingPartStr;
+};
+
+var reversePrefix222 = function (s, k) {
+    // Reverse first k characters and append the rest
+    return s.slice(0, k).split("").reverse().join("") + s.slice(k);
+};
