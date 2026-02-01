9122f5b7a4c0a698fb4268424dd1e3b1973ef986
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2026-02-01 13:18:31 +0530

    187-repeated-dna-sequences.js

--- 187-repeated-dna-sequences.js
+++ 187-repeated-dna-sequences.js
@@ -1,4 +1,32 @@
 var findRepeatedDnaSequences = function (s) {
+    // Set to store sequences seen for the first time
+    const once = new Set();
+
+    // Set to store sequences that appear more than once
+    const twice = new Set();
+
+    // Slide a window of length 10 over the string
+    for (let i = 0; i + 10 <= s.length; i++) {
+
+        // Extract the current 10-letter substring
+        const sub = s.slice(i, i + 10);
+
+        // If we've already seen this substring once,
+        // it means this is at least the second occurrence
+        if (once.has(sub)) {
+            twice.add(sub);
+        } else {
+            // First time seeing this substring
+            once.add(sub);
+        }
+    }
+
+    // Convert the set of repeated sequences to an array and return
+    return Array.from(twice);
+};
+
+
+var findRepeatedDnaSequences222 = function (s) {
     // Length of the input DNA string
     let n = s.length;
 
