f236d5b6efc145f4e3902cfc36790cf21a829e5c
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2026-02-01 13:31:21 +0530

    535-encode-and-decode-tinyurl.js

--- 535-encode-and-decode-tinyurl.js
+++ 535-encode-and-decode-tinyurl.js
@@ -1,3 +1,5 @@
+
+/*
 // Object used as an in-memory store to map short keys to long URLs
 let obj = {};
 
@@ -19,3 +21,31 @@ var decode = function (shortUrlKey) {
     // Look up and return the long URL associated with the given key
     return obj[shortUrlKey];
 };
+
+*/
+
+// Hash map to store shortUrl -> longUrl mapping
+let map = new Map();
+
+// Counter used to generate unique short URLs
+let id = 0;
+
+var encode = function (longUrl) {
+    // Convert the current ID to a string to form the short URL
+    const shortUrl = id.toString();
+
+    // Store the mapping from short URL to the original long URL
+    map.set(shortUrl, longUrl);
+
+    // Increment ID for the next encode operation
+    id++;
+
+    // Return the generated short URL
+    return shortUrl;
+};
+
+var decode = function (shortUrl) {
+    // Retrieve and return the original URL using the short URL
+    return map.get(shortUrl);
+};
+
