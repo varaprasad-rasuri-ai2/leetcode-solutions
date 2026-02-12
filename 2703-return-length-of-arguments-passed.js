e4eabce262fd2644768fd034968dfeff00785f3b
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2026-02-12 19:36:41 +0530

    2703-return-length-of-arguments-passed.js

--- 2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed.js
@@ -1,9 +1,3 @@
-/**
-* Write a function argumentsLength that returns the count of arguments passed to it.
- * @param {...(null|boolean|number|string|Array|Object)} args
- * @return {number}
- 
- */
 var argumentsLength = function(...args) {
     return args.length
 };
