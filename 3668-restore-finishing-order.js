45bf201f87a784eebb2da691556c1e5f435b806f
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2026-02-13 23:18:53 +0530

    3668-restore-finishing-order.js

--- 3668-restore-finishing-order.js
+++ 3668-restore-finishing-order.js
@@ -1,9 +1,11 @@
 var recoverOrder = function (finishOrderIds, friends) {
     let result = []
+    
     finishOrderIds.forEach((id, index) => {
         if (friends.includes(id)) {
             result.push(id)
         }
     })
+
     return result
 };
