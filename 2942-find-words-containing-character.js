453b17b0ff2ee8c48b4db80ed9633a67fc145df2
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2026-02-14 15:36:17 +0530

    2942-find-words-containing-character.js

--- 2942-find-words-containing-character.js
+++ 2942-find-words-containing-character.js
@@ -1,9 +1,11 @@
 var findWordsContaining = function (words, char) {
     let result = []
+    
     words.forEach((word, i) => {
         if (word.includes(char)) {
             result.push(i)
         }
     })
+
     return result
 };
