6d2abf847c2fd9ab48ca2bdb135175e6c4bf1f95
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2026-02-21 15:43:03 +0530

    1365-how-many-numbers-are-smaller-than-the-current-number.js

--- 1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -1,15 +1,19 @@
-var smallerNumbersThanCurrent = function(nums) {
-let outputArray = []
-for(i=0;i<nums.length;i++){
-  let count = 0
-  for(j=0;j<nums.length;j++){
-    if(nums[j] < nums[i]){
-      count= count+1
-      // console.log('count ', count)
+var smallerNumbersThanCurrent = function (nums) {
+  let outputArray = [];
+
+  for (i = 0; i < nums.length; i++) {
+    let count = 0;
+
+    for (j = 0; j < nums.length; j++) {
+      if (nums[j] < nums[i]) {
+        count = count + 1;
+        // console.log('count ', count)
+      }
     }
+
+    outputArray.push(count);
   }
-  outputArray.push(count)
-}
-// console.log('outputArray ', outputArray)
-return outputArray
+
+  // console.log('outputArray ', outputArray)
+  return outputArray;
 };
