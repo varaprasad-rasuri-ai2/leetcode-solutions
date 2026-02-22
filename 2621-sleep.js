c01e719a1b7d5e86da7135143e219b4b7539f7ae
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2026-02-22 08:45:50 +0530

    2621-sleep.js

--- 2621-sleep.js
+++ 2621-sleep.js
@@ -1,6 +1,7 @@
 // 'sleep' is an asynchronous function that pauses execution for a given time.
 async function sleep(millis) {
+
     // Return a Promise that resolves after 'millis' milliseconds.
     // setTimeout is used to delay the resolution of the promise.
-    return new Promise(resolve => setTimeout(resolve, millis))
+    return new Promise(resolve => setTimeout(resolve, millis));
 }
