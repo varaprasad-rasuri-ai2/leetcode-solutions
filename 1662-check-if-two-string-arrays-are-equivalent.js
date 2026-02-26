8e1a87f73fc5273cf28eb25bf6373e5dadbf3c30
varaprasad-rasuri-ai2 <rvaraprasad.ai2@gmail.com>
2026-02-26 19:06:26 +0530

    1662-check-if-two-string-arrays-are-equivalent.js

--- 1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent.js
@@ -9,6 +9,7 @@ var arrayStringsAreEqual = function (words1, words2) {
     // Step 2: Do the same for the second array
     let s2 = words2.reduce((fullStr, w) => fullStr + w, '');
 
+    
     // Step 3: Compare the two combined strings
     // If they are exactly the same, return true; otherwise, return false
     return s1 === s2;
